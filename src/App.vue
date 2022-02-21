6679403dd77506c261529e49295ae4f14f956134
Aasim-Omar <aasim.omar7@gmail.com>
2022-02-21 10:41:10 +0200

    New Push After Along

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <Sidebar v-if="largeScreen" />
-  <MediumSidebar v-if="mediumScreen"/>
+  <MediumSidebar v-if="mediumScreen" />
   <Smallbar v-if="smallScreen" />
   <div id="router-container">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="route" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Montserrat', sans-serif, "Segoe UI", Arial, sans-serif;
+  font-family: "Montserrat", sans-serif, "Segoe UI", Arial, sans-serif;
   text-align: center;
   color: #fff;
 }
@@ -23,6 +27,22 @@
   #router-container {
     margin-left: 250px;
   }
+}
+
+/* route transitions */
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(100px);
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(-100px);
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
 
